c7bbc546286a2ba238680f9839166ed66831bfd0
hedgehogs-mind <peter@hedgehogs-mind.com>
2021-02-01 21:28:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,117 @@
+# Javascript Cross Origin Frame RPC
+
+This tiny script enables you to call functions of an object, which sits in a (cross origin) frame, from an other frame.
+
+
+# Setup
+
+## 1. Choose channel name
+
+First you choose a channel name the __caller__ (or sender) and __executor__ (or receiver) communiate over. Lets say it's `yellow-post`.
+
+
+## 2. Set up executor/receiver side
+
+We need to set up the remote "service" / executor. First import the script:
+
+```
+<head>
+    <script src="cross_window_rpc.js"></script>
+</head>
+```
+
+Then put somewhere a script block or import your main js file. There you put the following code:
+
+```
+const targetObject = {
+    foo: function() {
+        return "bar"
+    },
+
+    greet: function(name) {
+        return `Hello ${name}! How are you today?`
+    }
+}
+
+const listener = setupCrossWindowExecutor(
+    targetObject,   // its methods will be executed
+    "yellow-post",  // channel name
+)
+```
+
+That's it!
+
+
+## 3. Set up caller side
+
+On the caller side, first import the script - just like on the receiver side:
+
+```
+<head>
+    <script src="cross_window_rpc.js"></script>
+</head>
+```
+
+Then you put somewhere a script block or extend your main js file. There are two ways to execute remote methods.
+
+### Proxy object
+
+This is the most convenient version. You create a proxy object, which acts like your remote object:
+
+```
+const service = createCrossWindowExecutorProxy(
+    // the target window (here: we are an iframe and call a service within the parent frame)
+    window.parent,   
+
+    // channel name
+    "yellow-post",   
+
+    // optional timeout in ms > default = 2000ms > on timeout, we get an error
+    1000             
+)
+
+service.foo()
+    .then(result => { console.log(result) })    // will output "bar"
+
+service.greet("Peter")
+    .then(result => { console.log(result) })    // will output "Hello Peter! How are you today?"
+
+service.someFunction()
+    .then(result => { console.log(result) })
+    .catch(error => { console.log(error) })     // will output "someFunction does not exist"
+```
+
+### "Native" call
+
+The same can be done using a call to `callCrossWindowExecutor(...)`:
+
+```
+// Equivalent for service.greet("Peter") from previous example
+
+callCrossWindowExecutor(
+    parent.window,
+    "yellow-post",
+
+    // method name
+    "greet",
+
+    // arguments as array
+    ["Peter"],
+
+    // optional timeout in ms > default = 2000ms
+    1000
+
+).then(result => {
+    // will output "Hello Peter! How are you today?"
+    console.log(result)    
+})    
+```
+
+# Background
+
+This implementation is based on following features:
+
+- `window.postMessage`
+- `Promise`
+- `Proxy` (if not available, use the "native" call method)
+- native JavaScript
---
+++ src/cross_window_rpc.js
@@ -0,0 +1,258 @@
+/*
+
+This script provides some function to call functions of an object which sits in an other (cross origin) frame. Function results are returned back.
+
+The whole concept is like RPC (remote procedure call), using Promises for asynchronous response handling.
+
+# Prerequisites:
+
+You must choose a communication channel name. Sender and receiver can only work together if they share the same communication channel name.
+
+
+# Sender side / iframe:
+
+There are two ways to call a remote object's function:
+
+    1. Using a super convenient and cool proxy! :)
+
+        You therefore do the following:
+
+
+            const proxy = createCrossWindowExecutorProxy(
+                parent.window, // the window object of the target frame,
+                "super-secret-channel", // the channel name
+                1000 // optional timeout in ms, default = 2000ms
+            )
+
+
+        You can then call any functions of the remote object by calling them on the proxy. Results are handled via a Promise:
+
+
+            proxy.convertToGirlName("Peter")
+                .then(result => { console.log(`Girl name for Peter: ${result}`) })  // may prints "Petra"
+                .catch(error => { console.log(error) })  // may prints "convertToGirlName is not a function"
+
+
+    2. Using native rpc call function:
+
+        You therefor call the following (equivalent to previous example):
+
+                callCrossWindowExecutor(
+                    parent.window,    // target window
+                    "super-secret-channel",  // channel name
+                    "convertToGirlName",    // function name
+                    [],     // method parameters as arguments
+                    1000    // optional timeout
+                )
+                    .then(result => { console.log(`Girl name for Peter: ${result}`) })  // may prints "Petra"
+                    .catch(error => { console.log(error) })  // may prints "convertToGirlName is not a function"
+
+
+# Receiver side
+
+On the receiver side you only need to create a handler which receives RPC requests and executes methods of a certain object:
+
+    const handler = {
+        convertToGirlName: function(name) {
+            if ( name === "Peter" ) return "Petra"
+            else if ( name === "Anton" ) return "Antonia"
+            else if ( name === "Michael" ) return "Michaela"
+
+            throw new Error("Can not convert name "+name+" to girl version.")
+        }
+    }
+
+    setupCrossWindowExecutor(
+        handler,        // the object whose methods will be tried to be called
+        "super-secret-channel"   // channel name
+    )
+
+
+*/
+
+
+
+// ######### RPC MESSAGES
+
+/*
+
+Sample request:
+
+{
+    commIndex: 0,
+    commChannel: "SCORM",
+    commType: "request",
+    method: "LMS_GET",
+    args: ["abc"] // optional
+}
+
+
+Sample response:
+
+{
+    commIndex: 0,
+    commChannel: "SCORM",
+    commType: "response",
+    method: "LMS_GET",
+    success: true
+    result: "Hello World" // this is optional
+}
+
+Or an error response:
+
+{
+    commIndex: 0,
+    commChannel: "SCORM",
+    commType: "response",
+    method: "LMS_GET",
+    success: false
+    result: "'apply' is not a function"
+}
+
+*/
+
+
+// ########## RPC SENDER SIDE
+
+function callCrossWindowExecutor(targetWindow, commChannel, method, argArray, timeout = 2000) {
+
+    // by this, each "request" has its own pseudo "id"
+
+    if ( !window.CROSS_COMM_INDEX ) {
+        window.CROSS_COMM_INDEX = 0
+    }
+
+    const callIndex = window.CROSS_COMM_INDEX++
+
+    return new Promise((resolve, reject) => {
+
+    let alreadyHandledByListener = false
+    let decayTimeout = null
+
+    function handleResponse(event) {
+    if ( event.data ) {
+        const response = event.data
+
+        // Check if response matches request
+        if ( response.commIndex === callIndex &&
+            response.commChannel === commChannel && 
+            response.commType === "response" &&
+            response.method === method
+        ) {
+        alreadyHandledByListener = true
+        if ( decayTimeout !== null ) clearTimeout(decayTimeout)
+
+        window.removeEventListener("message", handleResponse)
+
+        if ( response.success ) {
+            if ( response.result ) resolve(response.result)
+            else resolve()
+        } else {
+            reject(response.result ? response.result : new Error("Remote call returned with success = false. No error message given."))
+        }
+        }
+    }
+    }
+
+    window.addEventListener("message", handleResponse)
+
+    // message array = [FUNCTION_NAME, ...ARGS]
+    const message = {
+    commIndex: callIndex,
+    commChannel: commChannel,
+    commType: "request",
+    method: method,
+    args: !!argArray ? argArray : []
+    }
+
+    targetWindow.postMessage(message, "*")
+
+    if ( !alreadyHandledByListener ) {
+        decayTimeout = setTimeout(() => {
+            window.removeEventListener("message", handleResponse)
+
+            reject(new Error(`Remote call timeout. Got no response from called window after ${timeout} ms.`))
+        }, timeout)
+        }
+
+    })
+}
+
+function createCrossWindowExecutorProxy(targetWindow, commChannel, timeout = 2000) {
+return new Proxy({}, {
+    get: function(target, prop, receiver) {
+    return function() {
+        return callCrossWindowExecutor(
+        targetWindow,
+        commChannel,
+        prop,
+        [...arguments],
+        timeout
+        )
+    }
+    }
+})
+}
+
+
+
+
+
+
+// #########  RECEIVER SIDE
+
+function setupCrossWindowExecutor(callTarget, commChannel) {
+    const listener = function messageListener(event) {
+        const request = event.data
+
+        if ( request && 
+            request.commChannel === commChannel && 
+            request.commType === "request" &&
+            request.commIndex !== undefined ) {
+
+            let success = false
+            let result = null
+
+            if ( !request.method ) {
+                result = "method name was empty/not given"
+
+            } else {
+                const methodOfCallTarget = callTarget[request.method]
+
+                if ( methodOfCallTarget === undefined ) {
+                    result = `'${request.method}' does not exist`
+                } else if ( typeof methodOfCallTarget.apply !== "function" ) {
+                    result = `'${request.method}' is not a function`
+                } else {
+                    try {
+                        result = methodOfCallTarget.apply(
+                            callTarget, 
+                            Array.isArray(request.args) ? request.args : []
+                        )
+                        success = true
+                    } catch(error) {
+                        result = error.toString()
+                    }
+                }
+            }
+
+            const responseMessage = {
+                commIndex: request.commIndex,
+                commChannel: commChannel,
+                commType: "response",
+                method: request.method,
+                success: success,
+            }
+
+            if ( result !== undefined && result !== null ) {
+                responseMessage.result = result
+            }
+
+            event.source.postMessage(responseMessage, "*")
+        }
+    }
+
+    window.addEventListener("message", listener)
+
+    return listener
+}
